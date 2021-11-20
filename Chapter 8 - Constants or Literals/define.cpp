9d390b81cfe1ccc125990fbca92cc2d35773d5eb
krambo23 <taj.keshav@gmail.com>
2021-11-20 00:48:14 +0000

    Chapter 8

---
+++ Chapter 8 - Constants or Literals/define.cpp
@@ -0,0 +1,19 @@
+// pg 29 , 30
+
+#include <iostream>
+using namespace std;
+
+#define LENGTH 10
+#define WIDTH 5
+#define NEWLINE '\n'
+
+int main()
+{
+	int area;
+
+	area = LENGTH * WIDTH;
+	cout << area;
+	cout << NEWLINE;
+
+	return 0;
+}
---
+++ Chapter 8 - Constants or Literals/literals.cpp
@@ -0,0 +1,10 @@
+// pg 28 
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello\tWorld\n\n" ;
+	return 0;
+}
