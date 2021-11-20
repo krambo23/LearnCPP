bc042aae209b8254bfdc8a04351fb9ed165ea080
krambo23 <taj.keshav@gmail.com>
2021-11-20 00:39:57 +0000

    Chapter 6

---
+++ Chapter 6 - Variable Types/function_decl.cpp
@@ -0,0 +1,21 @@
+// pg 21
+
+#include <iostream>
+using namespace std;
+
+// function declaration
+int func();
+
+int main()
+{
+	// function call
+	int i = func();
+
+	return 0;
+}
+
+// function definition
+int func()
+{
+	return 0;
+}
